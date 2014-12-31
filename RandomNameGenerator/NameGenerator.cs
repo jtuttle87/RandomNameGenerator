3d26d94cc2ab78658f6c5a682b736d5c5493c2a1
jtuttle1987 <jtuttle1987@gmail.com>
2014-12-30 23:32:49 -0500

    Random name generator

---
+++ RandomNameGenerator/NameGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RandomNameGenerator
+{
+    public static class NameGenerator
+    {
+        private static Random _random = new Random();
+        private static List<string> _lastNames = new List<string>();
+        private static List<string> _dudes = new List<string>();
+        private static List<string> _ladies = new List<string>();
+        private static FileStream aFile;             
+        private static StreamReader sr;
+
+        public static string Generate(Gender gender)
+        {
+            return string.Format("{0} {1}", GenerateFirstName(gender), GenerateLastName());
+        }
+
+        private static void SetupLastName()
+        {
+            aFile = new FileStream("lastnames.csv", FileMode.Open);
+            sr = new StreamReader(aFile, true);
+            while (sr.Peek() >0 )
+            {
+                _lastNames.Add(sr.ReadLine());
+            }
+        }
+
+        private static void SetupDudes()
+        {
+            aFile = new FileStream("dudes.csv", FileMode.Open);
+            sr = new StreamReader(aFile, true);
+            while (sr.Peek() > 0)
+            {
+                _dudes.Add(sr.ReadLine());
+            }
+        }
+
+        private static void SetupLadies()
+        {
+            aFile = new FileStream("ladies.csv", FileMode.Open);
+            sr = new StreamReader(aFile, true);
+            while (sr.Peek() > 0)
+            {
+                _ladies.Add(sr.ReadLine());
+            }
+        }
+        
+        public static string GenerateLastName()
+        {
+            if (_lastNames.Count == 0)
+                SetupLastName();
+            return _lastNames[_random.Next(0, _lastNames.Count)];
+        }
+
+        private static string GenerateDudeName()
+        {
+            if (_dudes.Count == 0)
+                SetupDudes();
+            return _dudes[_random.Next(0, _dudes.Count)];
+        }
+
+        private static string GenerateLadieName()
+        {
+            if (_ladies.Count == 0)
+                SetupLadies();
+            return _ladies[_random.Next(0, _ladies.Count)];
+        }
+
+        public static string GenerateFirstName(Gender gender)
+        {
+            if (gender == Gender.Male)
+                return GenerateDudeName();
+            return GenerateLadieName();
+        }
+
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    
+}
---
+++ RandomNameGeneratorTests/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RandomNameGenerator;
+
+namespace RandomNameGeneratorTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ShouldGenerateLastName()
+        {
+            var lastName = NameGenerator.GenerateLastName();
+            Assert.AreEqual(string.IsNullOrEmpty(lastName), false);
+        }
+
+        [Test]
+        public void ShouldGenerateFirstName()
+        {
+            var firstName = NameGenerator.GenerateFirstName(Gender.Male);
+            Assert.AreEqual(string.IsNullOrEmpty(firstName), false);
+
+            firstName = NameGenerator.GenerateFirstName(Gender.Female);
+            Assert.AreEqual(string.IsNullOrEmpty(firstName), false);
+        }
+
+        [Test]
+        public void ShouldGenerateFullName()
+        {
+            var fullName = NameGenerator.Generate(Gender.Male);
+            Assert.AreEqual(string.IsNullOrEmpty(fullName), false);
+
+            fullName = NameGenerator.Generate(Gender.Female);
+            Assert.AreEqual(string.IsNullOrEmpty(fullName), false);
+        }
+    }
+}
